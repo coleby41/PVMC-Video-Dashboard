fab5b9da072288a6fffc873c9466e168c68383c2
Coleby Ludwig <clludwig947@gmail.com>
2025-12-16 23:14:55 -0500

    App Push

---
+++ scripts/phone-type-script.js
@@ -0,0 +1,13 @@
+<script>
+function downloadApp() {
+  const ua = navigator.userAgent || navigator.vendor || window.opera;
+
+  if (/iPad|iPhone|iPod/.test(ua)) {
+    window.open("https://apps.apple.com/app/idYOUR_APP_ID", "_blank");
+  } else if (/Android/.test(ua)) {
+    window.open("https://play.google.com/store/apps/details?id=YOUR.APP.ID", "_blank");
+  } else {
+    alert("Please visit the App Store or Google Play on your mobile device.");
+  }
+}
+</script>
