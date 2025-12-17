6343ec9acff95006a16772a731526d4d2cdc6476
Coleby Ludwig <clludwig947@gmail.com>
2025-12-16 23:30:40 -0500

    bug fixes

--- scripts/phone-type-script.js
+++ scripts/phone-type-script.js
@@ -2,9 +2,9 @@ function downloadApp() {
   const ua = navigator.userAgent || navigator.vendor || window.opera;
 
   if (/iPad|iPhone|iPod/.test(ua)) {
-    window.open("https://apps.apple.com/app/idYOUR_APP_ID", "_blank");
+    window.open("https://apps.apple.com/app/id6743939528", "_blank");
   } else if (/Android/.test(ua)) {
-    window.open("https://play.google.com/store/apps/details?id=YOUR.APP.ID", "_blank");
+    window.open("https://play.google.com/store/apps/details?id=com.renewedvision.propresenterremote&pcampaignid", "_blank");
   } else {
     alert("Please visit the App Store or Google Play on your mobile device.");
   }
