e77c21e49b50c272feda068f15c73ee5f7df469a
Coleby Ludwig <clludwig947@gmail.com>
2026-01-03 13:32:55 -0500

    Bug Fixes

--- scripts/campus-check2.js
+++ scripts/campus-check2.js
@@ -22,7 +22,7 @@ async function checkCampus() {
     } else {
       banner.innerHTML = `
         <span class="icon">✖</span>
-        You have left campus. This dashboard only works on the campus network. <a href="404.html">Click here to get help</a> | <a href="index.html">Go back to the home page</a>
+        You have left campus. This dashboard only works on the campus network. <a href="help.html">Click here to get help</a> | <a href="index.html">Go back to the home page</a>
       `;
     }
 
